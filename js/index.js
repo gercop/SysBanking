15c41afa289edbd0b6386d4dca1421c783fbe821
Ricardo Germanos <ricardo.germanos@dataa.com.br>
2022-04-02 12:34:56 -0300

    version 1.4

--- js/index.js
+++ js/index.js
@@ -1,18 +1,36 @@
 
-function requisitarBanking( ){        
+function requisitarBanking( url ){                
         
-        debugger; 
-        var data;
-        var requestURL = 'https://bb-api.concore.io/open-banking/channels/v1/branches';
-        var request = new XMLHttpRequest();
-        request.open('GET', requestURL);  
+        debugger;
+        var data = null;                
+        var requestURL = url;
+        var request = new XMLHttpRequest();        
+        request.open('GET', requestURL);        
         request.responseType = 'json';
-        request.send();      
-        request.onload = function() {
-                debugger;
+
+        /*request.withCredentials = true
+        request.setRequestHeader("Accept", "application/json");
+        request.setRequestHeader("Authorization", "string");
+        request.setRequestHeader("x-fapi-auth-date", "string");
+        request.setRequestHeader("x-fapi-customer-ip-address", "string");
+        request.setRequestHeader("x-fapi-interaction-id", "string");
+        request.setRequestHeader("x-customer-user-agent", "string");*/
+
+        request.send(data);      
+        request.onload = function() {                
                 data = request.response;
-                console.log(data);                
-        }
+                console.log(data);   
+                
+                //montar a tela para o cliente
+
+                //data_poupanca
+                //data_CDB
+                //if (data_CDB.projecao >  data_POUP.projecao) {
+
+                //} else {
+
+                //}
+        }        
 }
 
 
