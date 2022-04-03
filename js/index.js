fb68845aafd30cacf44ff4b91bee1c764f45c63a
Ricardo Germanos <ricardo.germanos@dataa.com.br>
2022-04-03 14:57:54 -0300

    V2.0

--- js/index.js
+++ js/index.js
@@ -1,4 +1,305 @@
+const milionario = 1000000.;
 var investiments = null;
+var nome, agencia, nroConta, digito; 
+    
+function requisitarDados(temNaAPIOpenBanking) {                
+        
+        if (temNaAPIOpenBanking) {
+                requisitarBanking(url);        
+        } else {
+                jsonHardCoded();
+        }
+        
+        nome     = investiments.data[0].civilName;
+        agencia  = investiments.data[0].accounts[0].accounts_list[0].branchCode;
+        nroConta = investiments.data[0].accounts[0].accounts_list[0].number;
+        digito   = investiments.data[0].accounts[0].accounts_list[0].checkDigit;
+        
+        document.getElementById("resultado").appendChild(criarTabelaId());
+        document.getElementById("resultado").appendChild(criarTabela(investiments));        
+}
+
+function criarSpan(texto) {
+
+        var legenda = document.createElement("span");
+        var texto_aux = document.createTextNode( texto );
+        legenda.appendChild( texto_aux );
+        return legenda;
+}
+
+function parseDate(texto) {
+
+        let dataDigitadaSplit = texto.split("/");
+      
+        let dia = dataDigitadaSplit[0];
+        let mes = dataDigitadaSplit[1];
+        let ano = dataDigitadaSplit[2];
+      
+      
+        if (ano.length < 4 && parseInt(ano) < 50) {
+          ano = "20" + ano;
+        } else if (ano.length < 4 && parseInt(ano) >= 50) {
+          ano = "19" + ano;
+        }
+        ano = parseInt(ano);
+        mes = mes - 1;
+      
+        return new Date(ano, mes, dia);
+      }
+
+function criarTabelaId() {
+
+        var tabelaId = document.createElement("table");        
+        var theadId  = document.createElement("thead");
+
+        var tr = document.createElement("tr");                
+        var t = document.createElement( thd(0) );
+        
+        let nomeAux = nome.split(" ");
+        var texto = document.createTextNode( "Seja bem-vindo " + nomeAux[0] + ",");
+        t.appendChild(texto);
+        tr.appendChild(t);              
+        
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( "Natureza: Conta Poupança");
+        t.appendChild(texto);
+        t.style.textAlign = "center";
+        tr.appendChild(t);                
+        
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( "Data: " + (new Date()).toLocaleString() );
+        t.appendChild(texto);
+        t.style.textAlign = "right";
+        tr.appendChild(t);         
+
+        tr.style.height = "20px";
+        tr.style.textAlign = "left";
+        
+        theadId.appendChild(tr);   
+
+        var tr = document.createElement("tr");                
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( "Agência: " + agencia );
+        t.appendChild(texto);      
+        tr.appendChild(t);           
+        
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( "Nro da Conta: " + nroConta + "-" + digito  );
+        t.appendChild(texto);      
+        tr.appendChild(t);           
+        
+        tr.style.height = "40px";
+        tr.style.textAlign = "left";
+        
+        tr.appendChild(t);
+        theadId.appendChild(tr);                
+        tabelaId.appendChild(theadId);
+
+        tabelaId.appendChild(theadId);
+
+        return tabelaId;
+}
+
+function criarTabela(conteudo) {        
+        
+        var saldoInicial = investiments.data[0].accounts[0].accounts_balances[0].availableAmount;
+        var interestRate1 = 0.61/100.; //Versão 4.0: Buscar API para obter tais informações
+        var interestRate2 = 0.81/100.; //Versão 4.0: Buscar API para obter tais informações
+        var depositosM1    = 400.;        
+        var depositosM2    = 800.;
+        
+        var tabela = document.createElement("table");        
+        var thead  = document.createElement("thead");
+        var tbody  = document.createElement("tbody");
+        
+        thead.appendChild(addCabecalho("Mês/ano","Poupança (R$)","CDB/RDB (R$)","CDB/RDB (R$)","CDB/RDB (R$)",true,false));
+        thead.appendChild(addCabecalho("Aportes Regulares","-","-","R$ 400,00","R$ 800,00",false,true));        
+        
+        var vlr = 0., saldoInicialAcumuladoP =0., saldoInicialAcumuladoR =0., saldoInicialAcumuladoD1 =0., saldoInicialAcumuladoD2 =0.;
+        var saldoInicialAcumuladoD2 = saldoInicialAcumuladoD1 = saldoInicialAcumuladoR = saldoInicialAcumuladoP = saldoInicial;        
+        var finalizarProjecao = false;
+
+        for (var i=1; i<=35*12 && !finalizarProjecao; i++) {                
+                
+                saldoInicialAcumuladoP  = (1.+interestRate1)*saldoInicialAcumuladoP;
+                saldoInicialAcumuladoR  = (1.+interestRate2)*saldoInicialAcumuladoR;
+                saldoInicialAcumuladoD1 = depositosM1+(1.+interestRate2)*saldoInicialAcumuladoD1;
+                saldoInicialAcumuladoD2 = depositosM2+(1.+interestRate2)*saldoInicialAcumuladoD2;                
+                
+                if ( mostraMesAno( (i) ) ) {                        
+                        
+                        var dtNext = somaMes(new Date(), i);              
+                        var tr = document.createElement("tr");                                                
+                        var t = document.createElement( thd(1) );
+                        var str_aux = (i>12) ? " (" + ((i-1)/12) + " anos)" : "";
+                        var texto = document.createTextNode( dataFormatadaMesAno(dtNext) + str_aux  );
+                        t.appendChild(texto);
+                        tr.appendChild(t);
+                
+                        vlr = saldoInicialAcumuladoP.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+                        var t = document.createElement( thd( !seTornouMilionario(saldoInicialAcumuladoP) ) );                        
+                        var texto = document.createTextNode( vlr );
+                        t.appendChild(texto);
+                        tr.appendChild(t);
+
+                        vlr = saldoInicialAcumuladoR.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+                        var t = document.createElement( thd( !seTornouMilionario(saldoInicialAcumuladoR) ) );
+                        var texto = document.createTextNode( vlr );
+                        t.appendChild(texto);
+                        tr.appendChild(t);                        
+
+                        vlr = saldoInicialAcumuladoD1.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+                        var t = document.createElement( thd( !seTornouMilionario(saldoInicialAcumuladoD1) ) );
+                        var texto = document.createTextNode( vlr );
+                        t.appendChild(texto);
+                        tr.appendChild(t);
+
+                        vlr = saldoInicialAcumuladoD2.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+                        var t = document.createElement( thd( !seTornouMilionario(saldoInicialAcumuladoD2) ) );
+                        var texto = document.createTextNode( vlr );                        
+                        t.appendChild(texto);                              
+                        tr.appendChild(t);
+                        if ( seTornouMilionario(saldoInicialAcumuladoD2) ) {
+                                tr.style.backgroundColor = "#add";
+                                tr.style.border = "2px solid blue"
+                                tr.style.height = "30px"                                
+                        }
+
+                        (i==1) ? thead.appendChild(tr):tbody.appendChild(tr);
+
+                        finalizarProjecao = seTornouMilionarioT(saldoInicialAcumuladoD2,saldoInicialAcumuladoD1,saldoInicialAcumuladoR,saldoInicialAcumuladoP);                                                                                
+                }
+        }
+        
+        tabela.appendChild(thead);
+        tabela.appendChild(tbody);
+
+        return tabela;
+}
+
+function pularEspacoComTexto(str1,str2,str3,str4) {
+        var tr = document.createElement("tr");                                                
+        var t = document.createElement( thd(1) );
+        var texto = document.createTextNode( str1 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+        
+        var tr = document.createElement("tr");                                                
+        var t = document.createElement( thd(1) );
+        var texto = document.createTextNode( str2 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+        
+        var tr = document.createElement("tr");                                                
+        var t = document.createElement( thd(1) );
+        var texto = document.createTextNode( str3 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+
+        var tr = document.createElement("tr");                                                
+        var t = document.createElement( thd(1) );
+        var texto = document.createTextNode( str4 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+        return tr;
+}
+                
+function seTornouMilionario(vlr) {
+        return (vlr > milionario);
+}
+
+function seTornouMilionarioT(vlr1,vlr2,vlr3,vlr4) {
+        return    (vlr1 > milionario) 
+               || (vlr2 > milionario) 
+               || (vlr3 > milionario) 
+               || (vlr4 > milionario);
+}
+
+
+function mostraMesAno(i) {
+        return (  ( (i > 0) && (i <= 12) ) 
+                 || ( i === (1+24)     )  //2 anos 
+                 || ( i === (1+5*12)   )  //5 anos
+                 || ( i === (1+10*12)  )  //5 anos
+                 || ( i === (1+20*12)  )  //10 anos  
+                 || ( i === (1+25*12)  )  //25 anos
+               )  
+}              
+
+function thd(i) {
+        return (i==0) ? "th" : "td";
+}
+
+function addCabecalho(col1, col2, col3, col4, col5, bordaSuperior, bordaInferior) {
+
+        var tr = document.createElement("tr");                
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( col1 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+        if (bordaSuperior)
+                tr.style.borderTop = "1px solid";   
+        if (bordaInferior)
+                tr.style.borderBottom = "1px solid";        
+             
+                
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( col2 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( col3 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( col4 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+
+        var t = document.createElement( thd(0) );
+        var texto = document.createTextNode( col5 );
+        t.appendChild(texto);
+        tr.appendChild(t);
+
+        return tr;        
+}
+
+function somaMes(data, qtdeMeses) {
+
+        var dtHoje = data;
+        var dia = 1;
+        var mes = dtHoje.getMonth() + qtdeMeses;
+        var ano = dtHoje.getFullYear();
+        var dtNext = new Date( ano, mes, dia);
+        console.log(dtNext);
+        return dtNext;
+}
+
+
+function dataFormatadaMesAno(data){        
+        mes  = (data.getMonth()+1).toString(); //+1 pois no getMonth Janeiro começa com zero.
+        mesF = (mes.length == 1) ? '0'+mes : mes;
+        anoF = data.getFullYear();
+        return mesF+"/"+anoF;
+}
+
+function requisitarOpenBanking(url){                
+
+        var data = null;                
+        var requestURL = url;
+        var request = new XMLHttpRequest();        
+        request.open('GET', requestURL);        
+        request.responseType = 'json';
+
+        request.send(data);      
+        request.onload = function() {                
+                debugger;
+                data = request.response;
+                console.log(data);                   
+        }                
+}
 
 function jsonHardCoded() {
 
@@ -412,145 +713,3 @@ function jsonHardCoded() {
                         }
                 }
 }
-    
-function requisitarDados(temNaAPIOpenBanking) {                
-        
-        if (temNaAPIOpenBanking) {
-                requisitarBanking(url);        
-        } else {
-                jsonHardCoded();
-        }
-
-        let nome = "Seja bem-vindo " + investiments.data[0].civilName + ",";
-        let agencia = "Agência: "+ investiments.data[0].accounts[0].accounts_list[0].branchCode;
-        let nroConta = "Nro da Conta: "+ investiments.data[0].accounts[0].accounts_list[0].number;
-        let digito = "Dígito da Conta: "+ investiments.data[0].accounts[0].accounts_list[0].checkDigit;
-
-        /*document.getElementById("resultado").appendChild(criarTabela([
-                ["id", "nome",     "idade"],
-                [1,    "matheus",  16],
-                [2,    "cristian", 16],
-                [3,    "pedro",    10],
-                [4,    "henrique", 10]
-        ]));*/
-
-        document.getElementById("resultado").appendChild(criarTabela(investiments));        
-}
-
-function criarSpan(texto) {
-
-        var legenda = document.createElement("span");
-        var texto_aux = document.createTextNode( texto );
-        legenda.appendChild( texto_aux );
-        return legenda;
-}
-
-function parseDate(texto) {
-
-        let dataDigitadaSplit = texto.split("/");
-      
-        let dia = dataDigitadaSplit[0];
-        let mes = dataDigitadaSplit[1];
-        let ano = dataDigitadaSplit[2];
-      
-      
-        if (ano.length < 4 && parseInt(ano) < 50) {
-          ano = "20" + ano;
-        } else if (ano.length < 4 && parseInt(ano) >= 50) {
-          ano = "19" + ano;
-        }
-        ano = parseInt(ano);
-        mes = mes - 1;
-      
-        return new Date(ano, mes, dia);
-      }
-
-function criarTabela(conteudo) {        
-
-        debugger;
-        var saldoInicial = investiments.data[0].accounts[0].accounts_balances[0].availableAmount;
-        var interestRate = 0.5/100.;
-        var dtHoje = Date();             
-
-        var tabela = document.createElement("table");        
-        var thead  = document.createElement("thead");
-        var tbody  = document.createElement("tbody");
-
-        var thd = function( i ) {
-                return (i==0) ? "th" : "td";
-        };        
-        
-        var tr = document.createElement("tr");                
-        var t = document.createElement( thd(0) );
-        var texto = document.createTextNode( 'Data Mês/Ano' );
-        t.appendChild(texto);
-        tr.appendChild(t);
-                
-        var t = document.createElement( thd(0) );
-        var texto = document.createTextNode( 'Rentabilização (R$)' );
-        t.appendChild(texto);
-        tr.appendChild(t);
-
-        thead.appendChild(tr);
-        
-        for (var i=1; i<=12; i++) {
-                var tr = document.createElement("tr");                
-        
-                var dtNext = somaMes(i);              
-                var saldoInicial = (1.+interestRate)*saldoInicial;
-                
-                var t = document.createElement( thd(1) );
-                var texto = document.createTextNode( dataFormatadaMesAno(dtNext) );
-                t.appendChild(texto);
-                tr.appendChild(t);
-                
-                var moeda = saldoInicial.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
-                var t = document.createElement( thd(1) );
-                var texto = document.createTextNode( moeda );
-                t.appendChild(texto);
-                tr.appendChild(t);
-
-                (i==1) ? thead.appendChild(tr):tbody.appendChild(tr);
-        }
-
-        tabela.appendChild(thead);
-        tabela.appendChild(tbody);
-
-        return tabela;
-}
-
-function somaMes(qtdeMeses) {
-
-        var dtHoje = new Date();
-        var dia = 1;
-        var mes = dtHoje.getMonth() + qtdeMeses;
-        var ano = dtHoje.getFullYear();
-        var dtNext = new Date( ano, mes, dia);
-        console.log(dtNext);
-        return dtNext;
-}
-
-
-function dataFormatadaMesAno(data){        
-        mes  = (data.getMonth()+1).toString(); //+1 pois no getMonth Janeiro começa com zero.
-        mesF = (mes.length == 1) ? '0'+mes : mes;
-        anoF = data.getFullYear();
-        return mesF+"/"+anoF;
-}
-
-function requisitarOpenBanking(url){                
-
-        var data = null;                
-        var requestURL = url;
-        var request = new XMLHttpRequest();        
-        request.open('GET', requestURL);        
-        request.responseType = 'json';
-
-        request.send(data);      
-        request.onload = function() {                
-                debugger;
-                data = request.response;
-                console.log(data);                   
-        }                
-}
-
